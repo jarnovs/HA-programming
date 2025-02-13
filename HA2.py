23d467736f4d533a7536811bcd4525ef0c1f0373
Sanjar <usalmazovich@gmail.com>
2025-02-13 22:32:18 +0600

    Design HA2

--- HA2.py
+++ HA2.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # Question 1
-# For creating series object in pandas we can use only one-dimensiaonal arrays (vectors)
+# For creating series object in pandas we can use only one-dimensional arrays (vectors)
 
 
 # Question 2
@@ -23,8 +23,8 @@ print(df)
 # Question 4
 exam_data = {
 	'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
-    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
-    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    	'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    	'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
 	'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
 }
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
